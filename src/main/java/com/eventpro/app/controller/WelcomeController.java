cea0c35bc1e69fe7f19bddab910bbdd79900e289
Chi Hoang <chhoang102@gmail.com>
2019-11-28 00:59:46 -0800

    Add API to get environment

--- src/main/java/com/eventpro/app/controller/WelcomeController.java
+++ src/main/java/com/eventpro/app/controller/WelcomeController.java
@@ -1,10 +1,21 @@
 package com.eventpro.app.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 public class WelcomeController {
+  @Value("${app.env}")
+  private String env;
+
+  @GetMapping("/env")
+  public String env() {
+    return env;
+  }
+
   @GetMapping("/healthCheck")
   public String healthCheck() {
     return "Health Check Okay";
