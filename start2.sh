6b51d72d510952b3cab7c20cf8f6ab718e1b4616
Chi Hoang <chhoang102@gmail.com>
2019-12-03 18:29:18 -0800

    Add script to start the backend app

---
+++ start2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
+echo $DATE_WITH_TIME
+
+kill -9 $(lsof -i:9000 -t) 2> /dev/null
+echo "Killed port 9000"
+mvn clean install
+
+nohup java -Dspring.profiles.active=prod -jar target/event-pro-0.0.1-SNAPSHOT.jar > ~/eventpro-${DATE_WITH_TIME}.log &
+
+echo "Started event-pro application"
