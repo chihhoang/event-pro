d326b49d2c625d35381b65d5435a3e8ad9189230
Chi Hoang <chhoang102@gmail.com>
2019-12-04 18:39:35 -0800

    Change script to port to 8443

--- start2.sh
+++ start2.sh
@@ -3,7 +3,7 @@
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
 echo $DATE_WITH_TIME
 
-kill -9 $(lsof -i:9000 -t) 2> /dev/null
+kill -9 $(lsof -i:8443 -t) 2> /dev/null
 echo "Killed port 9000"
 mvn clean install
 
