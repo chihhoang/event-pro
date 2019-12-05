637c7652132c182ba3c6a2f720da8b539d1d7ad6
Chi Hoang <chhoang102@gmail.com>
2019-12-04 19:10:59 -0800

    port 443 message

--- start2.sh
+++ start2.sh
@@ -4,7 +4,7 @@ DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
 echo $DATE_WITH_TIME
 
 kill -9 $(lsof -i:443 -t) 2> /dev/null
-echo "Killed port 9000"
+echo "Killed port 443"
 mvn clean install
 
 nohup java -Dspring.profiles.active=prod -jar target/event-pro-0.0.1-SNAPSHOT.jar > ~/eventpro-${DATE_WITH_TIME}.log &
