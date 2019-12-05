f128100a8bf1c1789a505fc66263838ed1857717
Manisha Yacham <manisha.yacham@sjsu.edu>
2019-12-04 19:59:11 -0800

    Update AppConfig.java

--- src/main/java/com/eventpro/app/config/AppConfig.java
+++ src/main/java/com/eventpro/app/config/AppConfig.java
@@ -6,20 +6,4 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @Configuration
 @EnableJpaAuditing
 public class AppConfig {
-  @Bean
-public EmbeddedServletContainerCustomizer containerCustomizer() {
-    return new EmbeddedServletContainerCustomizer() {
-        @Override
-        public void customize(ConfigurableEmbeddedServletContainer container) {
-            if (container instanceof TomcatEmbeddedServletContainerFactory) {
-                TomcatEmbeddedServletContainerFactory containerFactory =
-                        (TomcatEmbeddedServletContainerFactory) container;
-
-                Connector connector = new Connector(TomcatEmbeddedServletContainerFactory.DEFAULT_PROTOCOL);
-                connector.setPort(443);
-                containerFactory.addAdditionalTomcatConnectors(connector);
-            }
-        }
-    };
-}
 }
