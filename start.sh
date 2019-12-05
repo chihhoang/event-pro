2a3dfab88e07131204170342e52f152fc9b717cc
Chi Hoang <chhoang102@gmail.com>
2019-12-04 19:19:47 -0800

    Try https

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
 
-kill -9 $(lsof -i:443 -t) 2> /dev/null
+kill -9 $(lsof -i:8443 -t) 2> /dev/null
 nohup ./mvnw spring-boot:run -Dspring-boot.run.arguments=--spring.profiles.active=prod > ~/storagepro-${DATE_WITH_TIME}.log &
