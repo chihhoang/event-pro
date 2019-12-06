cb7bb7b12d6ef3d98a5a3ddeb6a33bbf0741b3bd
Chi Hoang <chhoang102@gmail.com>
2019-12-05 23:36:55 -0800

    Try 443 without ssl

--- start2.sh
+++ start2.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+echo "Remove old logs"
+rm -rf ~/eventpro-*
+
 DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
 echo $DATE_WITH_TIME
 
-kill -9 $(lsof -i:8443 -t) 2> /dev/null
-echo "Killed port 8443"
+kill -9 $(lsof -i:443 -t) 2> /dev/null
+echo "Killed port 443"
 mvn clean install
 
 nohup java -Dspring.profiles.active=prod -jar target/event-pro-0.0.1-SNAPSHOT.jar > ~/eventpro-${DATE_WITH_TIME}.log &
